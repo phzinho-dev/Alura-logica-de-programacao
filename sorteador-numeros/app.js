e9137f560666df0ea13f11c82cd374fbf4d94779
phzinho-dev <phelippevasquez@gmail.com>
2024-03-26 17:47:53 -0300

    pausa do dia

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,5 +1,12 @@
-function sortear(){
-  let quatidade = document.getElementById('quantidade');
-  let de = document.getElementById('de');
-  let ate = document.getElementById('ate');
+function sortear() {
+  let quatidade = parseInt(document.getElementById('quantidade').value);
+  let de = parseInt(document.getElementById('de').value);
+  let ate = parseInt(document.getElementById('ate').value);
+
+  let numero = numeroAleatorio(de, ate);
+  alert(numero);
+}
+
+function numeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
