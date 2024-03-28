c69da24b50a1e1137f9762a5bd69e2d14d6e1211
phzinho-dev <phelippevasquez@gmail.com>
2024-03-28 12:09:43 -0300

    projeto de games, e inicio carrinho

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,10 @@
+function adicionar() {
+  let produto = document.getElementById('produto').value;
+  let nomeProduto = produto.split('-')[0];
+  let valor = produto.split('R$')[1];
+  let quantidade = document.getElementById('quantidade');
+}
+
+function limpar() {
+
+}
